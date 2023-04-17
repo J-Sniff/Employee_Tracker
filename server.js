9fcf53023e22e7780fc03a5c4d9f9a1189b3fdcc
Jacob Sniff <jacobsniff18@gmail.com>
2023-04-17 16:46:15 -0400

    Established connection between database and server.

---
+++ server.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql2');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: ''
+    database: 'employee_db',
+});
+
+connection.connect ((err) => {
+    if (err) throw err;
+    console.log('Connected to employee_db');
+});
